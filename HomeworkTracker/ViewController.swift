8f19d8b306a308c8c7f6979b1a29ff163d49196a
bobbygabriel <bgabriel@bellarmine.edu>
2022-02-03 21:57:14 -0500

    Initial Commit

---
+++ HomeworkTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeworkTracker
+//
+//  Created by Bobby Gabriel on 2/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
